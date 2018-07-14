63c8b19e791656d21047f3ebd7b99b5834e48d1d
hazhir <hazhir.baxishi@gmail.com>
2018-07-14 16:29:35 +0430

    Edit parameter extractor output comments
    Do some stuffs to clean up param-extract.sh output comments.

--- param-extract.sh
+++ param-extract.sh
@@ -10,6 +10,7 @@ then
 	echo Error: Two few arguments!
 	exit 1
 fi
+echo Executing grep command...
 grepResult=$(grep -r $param $resultDir)
 if [[ -z "$grepResult" ]]
 then
@@ -17,6 +18,11 @@ then
 	exit 1
 fi
 prevWorkload="empty"
+echo Total number of grep output lines is $(echo "$grepResult" | wc -l)!
+declare -i i=0
+declare -i rem
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 while read -r line; do
 	currentWorkload=$(echo $line | cut -d '/' -f 3 | cut -d ':' -f 1)
 	if [ $currentWorkload != $prevWorkload ]; then
@@ -24,10 +30,16 @@ while read -r line; do
 	fi
 	paramValue=$(echo $line | awk -F $param '{print $2}' | cut -d '='  -f 2 | tr -d '[:space:]')
 	allOccurance="$allOccurance $paramValue"
-	prevWorkload=$currentWorkload	
+	prevWorkload=$currentWorkload
+	i=$i+1
+	rem=$i%100
+	if [ $rem -eq 0 ];then
+		echo -ne "Processing of ${RED}$i${NC} line is finished\033[0K\r"
+	fi
+
+echo ""
 done <<< "$grepResult"
-
-
+echo Calculating Min Max Average...
 #Second Part: Determine the WMMA string which is in format WMMA="$WMMA $prevWorkload $min $max $lastElement"
 WMMA=""
 re='^[+-]?[0-9]+\.?[0-9]*$'
@@ -46,17 +58,16 @@ for element in $allOccurance; do
 		noc=$noc+1
 	else
 		if [[ ! -z "${prevWorkload// }" ]]; then
-			echo extracting data for $prevWorkload output file...
 			WMMA="$WMMA $prevWorkload $min $max $lastElement $noc"
 		fi
 		min="9999999999999999999999999999999999999"
 		max="0"
 		prevWorkload=$element
 		noc=0
+		echo -e extracting data for ${RED}$element${NC}...
 	fi
 done
 
-echo extracting data for $prevWorkload output file...
 WMMA="$WMMA $prevWorkload $min $max $lastElement $noc"
 echo .
 echo .
@@ -64,7 +75,7 @@ echo .
 #Third Part: Print WMMA to a file in excel format
 prevWorkload=""
 excelEntry=""
-outputFile="$(echo $resultDir | cut -d '/' -f 2)-$param.xls"
+outputFile="excels/$(echo $resultDir | cut -d '/' -f 2)-$param.xls"
 echo Writing results to the $outputFile ...
 echo -e "$param\tMin\tMax\tAVG\tN_OC" > $outputFile
 for element in $WMMA; do
@@ -82,3 +93,4 @@ for element in $WMMA; do
 	fi
 done
 echo  -e $excelEntry >> $outputFile
+gdrive upload -p 1sDGgkLqud9zFEHZIQbIHKAKKOc7RhIjv $outputFile 
